3bdefb1b2a49f45a76dc8761ad85686b1a42ca6a
Gang Han <46079251+michaelhangang@users.noreply.github.com>
2021-10-03 15:18:56 -0400

    p18: Account bind

---
+++ srb/service-core/src/main/java/com/atguigu/srb/core/controller/api/UserBindController.java
@@ -0,0 +1,50 @@
+package com.atguigu.srb.core.controller.api;
+
+import com.atguigu.common.result.R;
+import com.atguigu.srb.base.util.JwtUtils;
+import com.atguigu.srb.core.hfb.RequestHelper;
+import com.atguigu.srb.core.pojo.vo.UserBindVO;
+import com.atguigu.srb.core.service.UserBindService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Api(tags = "会员账号绑定")
+@RestController
+@RequestMapping("/api/core/userBind")
+@Slf4j
+public class UserBindController {
+
+    @Resource
+    private UserBindService userBindService;
+
+    @ApiOperation("账户绑定提交数据")
+    @PostMapping("/auth/bind")
+    public R bind(@RequestBody UserBindVO userBindVO, HttpServletRequest request) {
+        String token = request.getHeader("token");
+        Long userId = JwtUtils.getUserId(token);
+        String formStr = userBindService.commitBindUser(userBindVO, userId);
+        return R.ok().data("formStr", formStr);
+    }
+
+    @ApiOperation("账户绑定异步回调")
+    @PostMapping("/notify")
+    public String notify(HttpServletRequest request) {
+        Map<String, Object> paramMap = RequestHelper.switchMap(request.getParameterMap());
+        // Validate sign
+        if(!RequestHelper.isSignEquals(paramMap)) {
+            return "fail";
+        }
+
+        userBindService.notify(paramMap);
+        return "success";
+    }
+}
--- srb/service-core/src/main/java/com/atguigu/srb/core/service/UserBindService.java
+++ srb/service-core/src/main/java/com/atguigu/srb/core/service/UserBindService.java
@@ -1,8 +1,13 @@
 package com.atguigu.srb.core.service;
 
 import com.atguigu.srb.core.pojo.entity.UserBind;
+import com.atguigu.srb.core.pojo.vo.UserBindVO;
 import com.baomidou.mybatisplus.extension.service.IService;
 
-public interface UserBindService extends IService<UserBind> {
+import java.util.Map;
 
+public interface UserBindService extends IService<UserBind> {
+    String commitBindUser(UserBindVO userBindVO, Long userId);
+
+    void notify(Map<String, Object> paramMap);
 }
